45aff868316c725ad681a5aed61a1e57b3a55259
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-09-24 17:07:28 +0300

    maze mutator

--- src/main/java/slava0135/fuzz3/maze/MazeMutator.java
+++ src/main/java/slava0135/fuzz3/maze/MazeMutator.java
@@ -1,9 +1,34 @@
 package slava0135.fuzz3.maze;
 
+import java.util.List;
+import java.util.Random;
+
 public class MazeMutator {
+    private static final Random random = new Random();
+    private static final List<String> dirs = List.of("U", "D", "L", "R");
+
+    private static String randomDir() {
+        return dirs.get(random.nextInt(4));
+    }
+
+    private static String append(String input) {
+        return input + randomDir();
+    }
+
+    private static String deleteLast(String input) {
+        return input.substring(0, Math.max(0, input.length() - 1));
+    }
+
+    private static String insert(String input) {
+        var splitIndex = random.nextInt(input.length());
+        return input.substring(0, splitIndex) + randomDir() + input.substring(splitIndex, input.length());
+    }
 
     public String mutate(String candidate) {
-        return candidate;
+        return switch (random.nextInt(3)) {
+            case 0 -> append(candidate);
+            case 1 -> deleteLast(candidate);
+            default -> insert(candidate);
+        };
     }
-    
 }
