b4106595dbd32c7230428798a56cec16780d67b0
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-09-27 14:56:50 +0300

    print N of trials

--- src/main/java/slava0135/fuzz3/maze/GreyBoxMazeFuzzer.java
+++ src/main/java/slava0135/fuzz3/maze/GreyBoxMazeFuzzer.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 public class GreyBoxMazeFuzzer {
     public static void main(String[] args) {
-        var fuzzer = new AdvancedMutationFuzzer(List.of(""), new MazeMutator(), new PowerSchedule(), 0, 3);
+        var fuzzer = new AdvancedMutationFuzzer(List.of(""), new MazeMutator(), new PowerSchedule(), 1, 3);
+        final int[] trials = new int[1];
         var runner = new FunctionRunner(s -> {
+            trials[0]++;
             var res = MazeGenerated.maze(s);
             if (res.contains("SOLVED")) {
-                System.out.println("\n-- FOUND SOLUTION --\n'" + s + "'");
+                System.out.println("\n-- FOUND SOLUTION (trials: " + trials[0] + ")--\n'" + s + "'");
                 System.exit(0);
             }
             if (res.contains("INVALID")) {
@@ -16,6 +18,6 @@ public class GreyBoxMazeFuzzer {
             }
             return "OK";
         });
-        fuzzer.fuzz(runner, 100000);
+        fuzzer.fuzz(runner, 30000);
     }
 }
