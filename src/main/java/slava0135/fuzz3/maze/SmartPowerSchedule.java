4c6827759f5b7cebb3b07c383c55810f8c3aca28
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-09-29 13:15:48 +0300

    update constants again

--- src/main/java/slava0135/fuzz3/maze/SmartPowerSchedule.java
+++ src/main/java/slava0135/fuzz3/maze/SmartPowerSchedule.java
@@ -10,11 +10,11 @@ public class SmartPowerSchedule extends PowerSchedule {
         for (Seed seed : population) {
             int pathId = pathIDGenerator.getPathID(seed.coverage);
             double frequency = getPathFrequency(pathId);
-            seed.setEnergy(1 / Math.pow(frequency, 10));
+            seed.setEnergy(1 / Math.pow(frequency, 1.2));
         }     
     }
 
     private int getPathFrequency(int pathId) {
-        return pathFrequency.getOrDefault(pathId, 1000);
+        return pathFrequency.getOrDefault(pathId, 1000000);
     }     
 }
