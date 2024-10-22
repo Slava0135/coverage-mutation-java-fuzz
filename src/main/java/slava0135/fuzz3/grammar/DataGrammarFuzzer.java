d29f59011c6b94953c9818182addacf8e06fcb4c
slava0135 <super.novalskiy_0135@inbox.ru>
2024-10-22 17:56:07 +0300

    this works

--- src/main/java/slava0135/fuzz3/grammar/DataGrammarFuzzer.java
+++ src/main/java/slava0135/fuzz3/grammar/DataGrammarFuzzer.java
@@ -3,7 +3,7 @@ package slava0135.fuzz3.grammar;
 public class DataGrammarFuzzer {
     public static void main(String[] args) {
         var grammar = DataGrammar.getBetterGrammar();
-        var fuzzer = new GrammarFuzzer(grammar, "<document>", 10, 20, false, false);
+        var fuzzer = new GrammarFuzzer(grammar, "<document>", 100, 1000, false, false);
         var tree = fuzzer.initTree();
         var expanded = fuzzer.expandTree(tree);
         System.out.println(expanded.treeToString());
